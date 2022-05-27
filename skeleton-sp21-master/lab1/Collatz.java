fbd6127e1ccd12556dfd46d4405ab965cc6cf2b6
timemachi <ksjdlhv@gmail.com>
2022-05-27 13:23:44 -0400

    delete .git

---
+++ skeleton-sp21-master/lab1/Collatz.java
@@ -0,0 +1,30 @@
+/** Class that prints the Collatz sequence starting from a given number.
+ *  @author YOUR NAME HERE
+ */
+public class Collatz {
+
+    /** Buggy implementation of nextNumber! */
+    public static int nextNumber(int n) {
+        if (n == 1){
+            return n;
+        } else if (n % 2 == 0) {
+            n = n / 2;
+            return n;
+        }else {
+            n = n * 3 + 1;
+            return n;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(n + " ");
+        while (n != 1){
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
+
+    }
+}
+
