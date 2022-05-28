26078c771b21880c356a17e9961c95644c71dc18
timemachi <ksjdlhv@gmail.com>
2022-05-28 14:04:03 -0400

    First question of disc05-OHQueue

---
+++ lectureCode-sp21-master/disc05/OHRequest/OHIterator.java
@@ -0,0 +1,35 @@
+package OHRequest;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class OHIterator implements Iterator<OHRequest> {
+    OHRequest curr;
+
+    public OHIterator(OHRequest queue) {
+        curr = queue;
+    }
+   public boolean isGood(String description) {
+        return description != null && description.length() > 5;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (curr != null && !isGood(curr.description)) {
+            curr = curr.next;
+        }
+        if (curr == null) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public OHRequest next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        OHRequest currRequest = curr;
+        curr = curr.next;
+        return currRequest;
+    }
+}
