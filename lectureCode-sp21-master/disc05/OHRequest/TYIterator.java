259312dfa4705b330a798b248b0f2c816c1a45ef
timemachi <ksjdlhv@gmail.com>
2022-05-29 16:07:38 -0400

    Add TYIterator for OHRequest

---
+++ lectureCode-sp21-master/disc05/OHRequest/TYIterator.java
@@ -0,0 +1,17 @@
+package OHRequest;
+
+public class TYIterator extends OHIterator {
+
+    public TYIterator(OHRequest queue) {
+        super(queue);
+    }
+
+    @Override
+    public OHRequest next() { // add new functions to superclass
+        OHRequest result = super.next();
+        if (result != null && result.description.contains("thank u")) {
+            result = super.next();
+        }
+        return result;
+    }
+}
