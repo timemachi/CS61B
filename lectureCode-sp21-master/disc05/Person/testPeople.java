7bcf0a88815a3f09a07c8c0b4839e123a76847a5
timemachi <ksjdlhv@gmail.com>
2022-05-29 17:07:37 -0400

    Last question

---
+++ lectureCode-sp21-master/disc05/Person/testPeople.java
@@ -0,0 +1,10 @@
+public class testPeople {
+    public static void main(String[] args) {
+        Person n = new Person("Neil", 12);
+        Person a = new Grandma("Ada", 60);
+        Person b = new Grandma("Beta", 60);
+        Grandma v = new Grandma("Vidya", 80);
+
+        (a).greet(v);
+    }
+}
